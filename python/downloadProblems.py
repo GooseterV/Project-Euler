fe309a391aec87d1bb52b937dd2a68fa6f687eaf
Gus <82774618+GooseterV@users.noreply.github.com>
2022-01-28 18:55:34 -0500

    Update downloadProblems.py
    Update

--- python/downloadProblems.py
+++ python/downloadProblems.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-for i in range(342, 773):
+for i in range(1, 773):
 	res = requests.get(f"https://projecteuler.net/minimal={i}")
 	with open(f"html/p{str(i).zfill(3)}.html", "w", encoding="utf-8") as pfile:
 		soup = BeautifulSoup(res.text, "html.parser")
