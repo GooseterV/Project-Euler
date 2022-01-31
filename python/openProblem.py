f5ccb95330c8a9301d792a3b68586fd2810afff9
Gus <82774618+GooseterV@users.noreply.github.com>
2022-01-31 11:08:11 -0500

    Update problem manager python file.

--- python/openProblem.py
+++ python/openProblem.py
@@ -1,4 +1,36 @@
 import webbrowser
 import os
+from numpy import True_
+import requests
+import bs4 as BeautifulSoup
+def downloadMinimal():
+	for i in range(1, 773):
+		res = requests.get(f"https://projecteuler.net/minimal={i}")
+		with open(f"html/p{str(i).zfill(3)}-MINIMAL.html", "w", encoding="utf-8") as pfile:
+			soup = BeautifulSoup(res.text, "html.parser")
+			pfile.write(soup.prettify()) 
+	DOWNLOADED = True_
 
-webbrowser.open(os.path.abspath(f'html/p{input("Problem Number? ").zfill(3)}.html'))
+def downloadAll():
+	for i in range(1, 773):
+		res = requests.get(f"https://projecteuler.net/problem={i}")
+		with open(f"html/p{str(i).zfill(3)}.html", "w", encoding="utf-8") as pfile:
+			soup = BeautifulSoup(res.text, "html.parser")
+			pfile.write(soup.prettify()) 
+	DOWNLOADED = True_
+
+minimal = True
+DOWNLOADED = False
+if not DOWNLOADED:
+	if minimal:
+		downloadMinimal()
+	elif not minimal:
+		downloadAll()
+if minimal:
+	fp = f'html/p{input("Problem Number? ").zfill(3)}-MINIMAL.html'
+else:
+	fp = f'html/p{input("Problem Number? ").zfill(3)}.html'
+if not os.path.exists(fp):
+	print("File is missing: " + fp)
+else:
+	webbrowser.open(os.path.abspath(fp))
