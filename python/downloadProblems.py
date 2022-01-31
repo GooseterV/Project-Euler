9534787407473d064094547eb7c83a28acced837
Gus <82774618+GooseterV@users.noreply.github.com>
2022-01-31 11:23:47 -0500

    Updates

--- python/downloadProblems.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-for i in range(1, 773):
-	res = requests.get(f"https://projecteuler.net/minimal={i}")
-	with open(f"html/p{str(i).zfill(3)}.html", "w", encoding="utf-8") as pfile:
-		soup = BeautifulSoup(res.text, "html.parser")
-		pfile.write(soup.prettify()) 
